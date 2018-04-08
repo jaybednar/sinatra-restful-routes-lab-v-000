8b371e5b4cb3ee26c672f0e5b2ba45673efc2914
Jay Bednar <ironviralmedia@gmail.com>
2018-04-08 00:00:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/recipes/:id/edit' do
-    @recipe = Recipe.find_by(params[:id])
+    @recipe = Recipe.find_by_id(params[:id])
     erb :'recipes/edit'
   end
 
